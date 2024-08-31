c4f4f8db182d8cd57c53f4623cafc0901367e71f
oriana-monaldi <orimonaldi@gmail.com>
2024-08-31 20:39:27 -0300

    v2

--- Bolt/src/App.jsx
+++ Bolt/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import './assets/components/Header.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    
     </>
   )
 }
