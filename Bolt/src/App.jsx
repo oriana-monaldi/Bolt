68e2a68bb74e1609ba957d0bf849375a5693ff7c
oriana-monaldi <orimonaldi@gmail.com>
2024-08-31 19:05:04 -0300

    v1

---
+++ Bolt/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './assets/components/Header.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
