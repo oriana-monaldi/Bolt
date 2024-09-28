5f33e4ff50a0846d8e323c6acb1d789e546e6a1e
oriana-monaldi <orimonaldi@gmail.com>
2024-09-28 13:53:25 -0300

    itemDetail

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Button() {
     return (
         <div className='bg-black text-xl text-white w-30 h-8 text-center rounded-md'>
-            <Link to="#">Comprar</Link> 
+            <Link to="#" className='p-2'>Comprar</Link> 
         </div>
     );
 }
