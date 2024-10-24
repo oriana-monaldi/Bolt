60b67fc713306db5de16f0d9efbffefc84ce8274
oriana-monaldi <orimonaldi@gmail.com>
2024-10-24 20:04:30 -0300

    buttons

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -24,7 +24,6 @@ return (
 			+
 		</button>
 		</div>
-		
 		<button
 		onClick={() => onAdd(count)}
 		className="bg-black text-white px-4 py-2 rounded"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,18 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav className=" text-black shadow-lg">
-      <div className='container mx-auto flex items-center justify-between px-4 py-3'>
-        {/* Logo */}
-        <Link to='/' className="flex items-center">
+      <div className='container mx-auto flex items-center justify-between px-4 py-3'>        <Link to='/' className="flex items-center">
           <img 
             className='w-12 h-12 rounded-full object-cover' 
             src={logo} 
             alt="Logo" 
           />
         </Link>
-
-        {/* Navigation Links */}
         <ul className='flex items-center space-x-8'>
           <li>
             <Link 
               to="/Remeras" 
-              className="hover:text-gray-300 transition-colors duration-200 text-lg"
+              className="hover:text-indigo-400 transition-colors duration-200 text-lg"
             >
               Remeras
             </Link>
@@ -29,7 +25,7 @@ function Navbar() {
           <li>
             <Link 
               to="/Shorts" 
-              className="hover:text-gray-300 transition-colors duration-200 text-lg"
+              className="hover:text-indigo-400 transition-colors duration-200 text-lg"
             >
               Shorts
             </Link>
@@ -37,14 +33,13 @@ function Navbar() {
           <li>
             <Link 
               to="/Pantalones" 
-              className="hover:text-gray-300 transition-colors duration-200 text-lg"
+              className="hover:text-indigo-400 transition-colors duration-200 text-lg"
             >
               Pantalones
             </Link>
           </li>
         </ul>
 
-        {/* Cart Widget */}
         <div className="flex items-center">
           <CartWidget />
         </div>
